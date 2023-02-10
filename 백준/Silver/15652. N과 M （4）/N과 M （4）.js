7e7c9a027b7b8c6119ec9d3d22e7166caacdaa4e
eunoo <95074093+eunoo1995@users.noreply.github.com>
2023-02-10 15:24:38 +0900

    [Silver III] Title: N과 M (4), Time: 304 ms, Memory: 25400 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.js
@@ -0,0 +1,39 @@
+let fs = require("fs");
+const input = fs
+  .readFileSync(
+    process.platform === "linux"
+      ? "/dev/stdin"
+      : "/Users/test/Desktop/beakjun/input.txt"
+  )
+  .toString()
+  .trim()
+  .split(" ")
+  .map((item) => +item);
+
+const solution = (n, m) => {
+  const arr = Array.from({ length: n }).map((_, i) => i + 1);
+  const result = getPermutations(arr, m)
+    .map((item) => item.join(" "))
+    .join("\n");
+  console.log(result);
+};
+
+const getPermutations = (arr, num) => {
+  const results = [];
+  if (num === 1) return arr.map((v) => [v]);
+
+  arr.forEach((fixed, index, origin) => {
+    const permutations = getPermutations(
+      origin.filter((item) => item >= fixed),
+      num - 1
+    );
+
+    const attached = permutations.map((v) => [fixed, ...v]);
+
+    results.push(...attached);
+  });
+
+  return results;
+};
+
+solution(...input);
